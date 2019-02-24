0b220758dbf9c8e92d0164d76af973ba4f617bea
whjin <1414848321@qq.com>
2019-02-24 21:43:41 +0800

    :pencil2:初次提交代码

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# make-quasar
-使用Quasar设计旅游网站
+# Quasar App
+
+> 使用文档：[使用Quasar设计Material和IOS风格的响应式网站](https://whjin.github.io/blog/2019/02/23/%E4%BD%BF%E7%94%A8Quasar%E8%AE%BE%E8%AE%A1Material%E5%92%8CIOS%E9%A3%8E%E6%A0%BC%E7%9A%84%E5%93%8D%E5%BA%94%E5%BC%8F%E7%BD%91%E7%AB%99/)
---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,85 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="primary"
+        text-color="pink-13"
+        inverted>
+
+        <q-toolbar-title>
+          好玩旅游网
+          <div slot="subtitle">WeFunWeb</div>
+        </q-toolbar-title>
+        <div class="gt-xs">
+          <q-btn class="q-mr-md" flat>精选景点</q-btn>
+          <q-btn class="q-mr-md" flat>美食地图</q-btn>
+          <q-btn class="q-mr-md" flat>夜宿晓行</q-btn>
+          <q-btn class="q-mr-md" flat>注册</q-btn>
+          <q-btn class="q-mr-md" flat>登录</q-btn>
+        </div>
+        <div class="lt-sm">
+          <q-btn
+            icon="fas fa-bars"
+            flat
+            @click="rightDrawerOpen=true"/>
+        </div>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-layout-drawer
+      v-model="rightDrawerOpen"
+      content-class="bg-grey-10"
+      side="right">
+      <q-list
+        no-border
+        link
+        inset-delimiter
+        dark>
+        <q-list-header>好玩旅游网</q-list-header>
+        <q-item>
+          <q-item-side icon="fas fa-search-location"/>
+          <q-item-main label="精选景点"/>
+        </q-item>
+        <q-item>
+          <q-item-side icon="fas fa-map-marked-alt"/>
+          <q-item-main label="美食地图"/>
+        </q-item>
+        <q-item>
+          <q-item-side icon="fas fa-utensils"/>
+          <q-item-main label="夜宿晓行"/>
+        </q-item>
+        <q-item>
+          <q-item-side icon="fas fa-sign-in-alt"/>
+          <q-item-main label="登录"/>
+        </q-item>
+        <q-item>
+          <q-item-side icon="far fa-edit"/>
+          <q-item-main label="注册" sublabel="忘记密码？"/>
+        </q-item>
+      </q-list>
+    </q-layout-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+  import {openURL} from 'quasar'
+
+  export default {
+    name: 'MyLayout',
+    data() {
+      return {
+        rightDrawerOpen: false
+      }
+    },
+    methods: {
+      openURL
+    }
+  }
+</script>
+
+<style>
+</style>
